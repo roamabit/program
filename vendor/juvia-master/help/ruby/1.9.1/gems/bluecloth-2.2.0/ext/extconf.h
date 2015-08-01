b5389c09633254788e494c56bbdde795474a8c68
Michael Rotakhin <Rotakhin@gmail.com>
2015-07-31 21:47:45 -0700

    Remove Juvia from Vendor

--- vendor/juvia-master/help/ruby/1.9.1/gems/bluecloth-2.2.0/ext/extconf.h
+++
@@ -1,9 +0,0 @@
-#ifndef EXTCONF_H
-#define EXTCONF_H
-#define HAVE_SRAND 1
-#define HAVE_RAND 1
-#define HAVE_STRCASECMP 1
-#define HAVE_STRNCASECMP 1
-#define HAVE_MKDIO_H 1
-#define HAVE_RUBY_ENCODING_H 1
-#endif
