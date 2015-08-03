0ae9d19575b84e66260a7e9ae81216fe470cdbc0
Michael Rotakhin <Rotakhin@gmail.com>
2015-08-03 12:15:15 -0700

    Add missing 'end' to Simtags_controller

--- app/controllers/simtags_controller.rb
+++ app/controllers/simtags_controller.rb
@@ -1,9 +1,11 @@
 class SimtagsController < ApplicationController
 
 		def tagsimvol
-		
+
 	 @newsimvol = simvol_params.newsimvol
-	 Simvol.create(:name => @newsimvol) 
+	 Simvol.create(:name => @newsimvol)
 	 @simvol.tags << Simvol.find_by_name(@newsimvol)
-	
+
 	end
+
+end
