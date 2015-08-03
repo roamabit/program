f3e6bc875ed75a8cd54622c6a00a2bb8bee87b76
Michael Rotakhin <Rotakhin@gmail.com>
2015-08-03 12:39:01 -0700

    remove extra 'END' from extendedprojects.rb

--- app/models/extendedprojects.rb
+++ app/models/extendedprojects.rb
@@ -2,11 +2,13 @@ module extendedprojects
 		def recent
 			where("created_at > recentdate.@rdate").reverse
 			end
-	
+
 	def recentdate
 		@rdate=Date-Day(7)
 		end
+
 		def popular
 			end
-		end
+
+
 end
