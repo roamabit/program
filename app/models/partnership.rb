244dd4948556d490de164d4ea5fd6ff4655cbbff
Michael Rotakhin <Rotakhin@gmail.com>
2015-08-11 07:12:06 -0700

    Hide Datetime field on problems

--- app/models/partnership.rb
+++ app/models/partnership.rb
@@ -1,17 +1,19 @@
 class Partnership < ActiveRecord::Base
 	#default_scope {where( :user_id => current_user.id )}
-	
+
 	#scope: :c_user => { where(:user_id => current_user.id) }
 	#scope :c_user, -> { where(user_id: current_user.id) }
   	#attr_accessor :current_user
+
 	belongs_to :project
 	belongs_to :user
 	has_many :comments, :as => :commentable, dependent: :destroy
 	acts_as_commentable
 	acts_as_votable
+
 	#default_scope -> {where :user_id == current_user.id}
-	
-	
+
+
 	validates :project_id, :presence => true
 	validates :user_id, :presence => true
 	validates :role, :presence => true
