f9b3e9e6c8724baa54e8ca61d5c0a8db7a5fc6f9
Michael Rotakhin <Rotakhin@gmail.com>
2015-08-13 21:12:43 -0700

    change stuff...

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-//= require underscore
 //= require gmaps/google
+= require underscore
+= require gmaps4rails/gmaps4rails.base
+= require gmaps4rails/gmaps4rails.googlemaps
