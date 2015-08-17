d1845617bf69daae76f626b084edabc1acfbfd03
Michael Rotakhin <Rotakhin@gmail.com>
2015-08-17 07:38:27 -0700

    Add asset precompilation to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Precompile Javascript assests
+  config.assets.precompile += %w( *.js )
+
 end
