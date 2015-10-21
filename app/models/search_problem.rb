515ec3f7abc5b925edab5cb4ded1d67e282b5c1f
Michael Rotakhin <Rotakhin@gmail.com>
2015-10-20 17:28:04 -0700

    Clean up project tables. Remove JS from application.js and move to the map partial.

--- app/models/search_problem.rb
+++ app/models/search_problem.rb
@@ -1,18 +1,18 @@
 class SearchProblem < ActiveRecord::Base
-	
+
 	has_many :comments, :as => :commentable, dependent: :destroy
 	acts_as_commentable
-	
+
 	geocoded_by :location   # can also be an IP address
-after_validation :geocode,   :if => :location_change       # auto-fetch coordinates
-	
-	
+	after_validation :geocode,   :if => :location_change       # auto-fetch coordinates
+
+
 	def problems
 		@problems ||= find_problems
 	end
 
 private
-	
+
 	def find_problems
 		problems = Problem.order(:statement)
 		problems = problems.where("statement like ?", "%#{keywords}%") if keywords.present?
@@ -20,6 +20,6 @@ private
 		problems = problems.where("published_at <= ?", date_max) if date_max.present?
 		problems = problems.near(location,location_distance)
 		problems
-	end 
-	
+	end
+
 end
