2ec865a20877e931ae405b07a2e86163b3c8ce1c
Kraiyanat Chomchumpol <kraiyanat@gmail.com>
2015-11-21 19:30:47 -0800

    Added feature regitration email comfirmation (add field to db , comfig users model)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
   #,:confirmable
   #, :lockable, :timeoutable, :omniauthable
