85ad21a259a10736a7c37a8e4ff96992986a9ab8
Kraiyanat Chomchumpol <kraiyanat@gmail.com>
2015-12-07 21:38:42 -0800

    Changed sender to eightkminc@gmail.com and :host => 'www.eightkm.com'

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   #config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   
   # Email
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'www.eightkm.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
