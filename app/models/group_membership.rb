5e3e61354321b8c0aa9e04514cda2b92f31bdafe
Michael Rotakhin <Rotakhin@gmail.com>
2016-01-11 12:47:29 -0800

    Add more styling and less Code

--- app/models/group_membership.rb
+++ app/models/group_membership.rb
@@ -1,15 +1,4 @@
 class GroupMembership < ActiveRecord::Base
   groupify :group_membership
 
-
-
-def mship(id, type)
-  group_memberships(member_id: id, member_type: type)
-end
-
-def new_user_member
-  current_user.groups << @group
-end
-
-
 end
