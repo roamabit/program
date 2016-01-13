610989a76064a2d885fdb6e534f56a9b91b8371a
Michael Rotakhin <Rotakhin@gmail.com>
2016-01-12 17:44:03 -0800

    Fix the activities Index

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,6 @@
 class ActivitiesController < ApplicationController
   def index
-
-  @activities = PublicActivity::Activity.order("created_at desc").where(owner_id: current_user.friends, owner_type: "User")
+  @activities = PublicActivity::Activity.order("created_at desc").where(current_user.friends, owner_type: "User")
 
   end
 end
