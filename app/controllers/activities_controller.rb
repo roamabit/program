a379f2fbc48ae4d565b1c618f1926a758704e012
Michael Rotakhin <Rotakhin@gmail.com>
2016-01-12 18:03:34 -0800

    Fix index partial on Pub_activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,8 @@
 class ActivitiesController < ApplicationController
-  def index
-  @activities = PublicActivity::Activity.order("created_at desc").where(current_user.friends, owner_type: "User")
 
+  def index
+
+  @activities = PublicActivity::Activity.order("created_at desc").where(current_user.friends, owner_type: "User")
+  #warning - This is duplicated in the user_controller#show need to build ref insted.
   end
 end
