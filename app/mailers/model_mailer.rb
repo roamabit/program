086e3ebb00f851fb984c61ab7f906a007c939c14
Michael Rotakhin <Rotakhin@gmail.com>
2016-04-15 14:48:14 -0700

    mgsetup 2

--- app/mailers/model_mailer.rb
+++ app/mailers/model_mailer.rb
@@ -1,10 +1,12 @@
 class ModelMailer < ActionMailer::Base
   default from: "Eliza@mg.eightkm.com"
 
-def new_record_notification(recepient, record)
+def new_record_notification(recepient, record, note, url)
   @greeting = "Hi,"
   @record = record
   @recepient = recepient
+  @mailer_note = note
+  @url = url
   mail to: @recepient, subject: "Success! You did it."
 end
 end
